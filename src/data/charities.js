8a4a73129a65a91bdd123ef5b992dd5d4dc7f8b0
Are1PM <arwanpriantomangidi@gmail.com>
2023-08-15 23:26:52 +0800

    [UPDATE] Add dummy data

---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: "First Charity Project",
+      category: "Money",
+      thumbnail:
+        "https://live.staticflickr.com/65535/53115213951_c38c208743_b.jpg",
+      pledged: 0,
+      target: 1000000,
+      date_end: +new Date("10 August 2023"),
+      profile_photo:
+        "https://live.staticflickr.com/2588/4212445717_75b2dd1ea3_b.jpg",
+      profile_name: "Bejamin Busche",
+      no_pledges: 0,
+    },
+  ],
+};
