f8b97908e8c5631b67a74a119d84897fd68377ba
Are1PM <arwanpriantomangidi@gmail.com>
2023-08-16 11:18:06 +0800

    [UPDATE] Add component CharityList and use dummy data

---
+++ src/components/CharityList.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let charities;
+</script>
+
+<div>
+    <h1>Daftar Charity</h1>
+    {#each charities as charity}
+    <ul>
+        <li>{charity.title}</li>
+    </ul>
+    {/each}
+</div>
+
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,4 +1,9 @@
 <script>
+	import CharityList from "../components/CharityList.svelte";
+	import { charities } from "../data/charities";
+
 	export let name;
 </script>
 <h1>{name}</h1>
+<CharityList {charities} />
+
