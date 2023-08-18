5da82c2ce42dc2e42b1880daf4644a41ca78bb3a
Are1PM <arwanpriantomangidi@gmail.com>
2023-08-18 22:57:45 +0800

    [UPDATE] change path link from index.html to root path

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,19 +4,19 @@
         <nav class="xs-menus">
             <div class="nav-header">
                 <div class="nav-toggle"></div>
-                <a href="index.html" class="nav-logo">
+                <a href="/" class="nav-logo">
                     <img src="assets/images/logo.png" alt="">
                 </a>
             </div><!-- .nav-header END -->
             <div class="nav-menus-wrapper row">
                 <div class="xs-logo-wraper col-lg-2 xs-padding-0">
-                    <a class="nav-brand" href="index.html">
+                    <a class="nav-brand" href="/">
                         <img src="assets/images/logo.png" alt="">
                     </a>
                 </div><!-- .xs-logo-wraper END -->
                 <div class="col-lg-7">
                     <ul class="nav-menu">
-                        <li><a href="index.html">home</a></li>
+                        <li><a href="/">home</a></li>
                         <li><a href="about.html">about</a></li>
                         <li><a href="contact.html">Contact</a></li>
                     </ul><!-- .nav-menu END -->
