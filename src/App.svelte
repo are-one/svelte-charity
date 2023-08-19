1a82fb7e043f7b813928947981048af829ba7b05
Are1PM <arwanpriantomangidi@gmail.com>
2023-08-19 22:14:03 +0800

    [UPDATE] add page router

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,23 @@
 <script>
+	import router from "page";
 	import Home from "./pages/Home.svelte";
 	import About from "./pages/About.svelte";
 	import Contact from "./pages/Contact.svelte";
 
+	
+	let page;
+	
+	router('/', () => (page = Home));
+	router('/about', () => (page = About));
+	router('/contact', () => (page = Contact));
+	
+	router();
+
 </script>
 
-<!-- <Home /> -->
-<!-- <About /> -->
-<Contact />
+<!-- <svelte:head>
+	<script src="assets/js/main.js"></script>
+</svelte:head> -->
+
+
+<svelte:component this={page} />
