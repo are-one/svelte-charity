32ed7a9d354a63cd48820e9ecc2e5fc58ed6bb91
Are1PM <arwanpriantomangidi@gmail.com>
2023-08-24 23:57:20 +0800

    [UPDATE] change image local charities data

--- src/data/charities.js
+++ src/data/charities.js
@@ -1,32 +1,43 @@
 module.exports = {
   charities: [
     {
+      category: "Business",
+      date_end: 1613581200000,
       id: 1,
-      title: "First Charity Project",
-      category: "Money",
-      thumbnail:
-        "https://live.staticflickr.com/65535/53115213951_c38c208743_b.jpg",
-      pledged: 99000,
-      target: 100000,
-      date_end: +new Date("10 August 2023"),
+      pledged: 0,
+      profile_name: "Emily Waldon",
       profile_photo:
-        "https://live.staticflickr.com/2588/4212445717_75b2dd1ea3_b.jpg",
-      profile_name: "Bejamin Busche",
-      no_pledges: 0,
+        "https://live.staticflickr.com/3172/2972948615_f60cbd939d_z.jpg",
+      target: 15670000,
+      thumbnail:
+        "https://live.staticflickr.com/2569/4013944382_469787def0_z.jpg",
+      title: "Keep William Writing",
     },
     {
+      category: "Education",
+      date_end: 1608829200000,
       id: 2,
-      title: "Second Charity Project",
-      category: "Money",
-      thumbnail:
-        "https://live.staticflickr.com/65535/53115213951_c38c208743_b.jpg",
-      pledged: 0,
-      target: 100000,
-      date_end: +new Date("10 August 2023"),
+      pledged: 55200000,
+      profile_name: "Kate Ludlow",
       profile_photo:
-        "https://live.staticflickr.com/2588/4212445717_75b2dd1ea3_b.jpg",
-      profile_name: "Bejamin Busche",
-      no_pledges: 0,
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Kate_Miller-Heidke_%288482043582%29.jpg/405px-Kate_Miller-Heidke_%288482043582%29.jpg",
+      target: 101500000,
+      thumbnail:
+        "https://live.staticflickr.com/6200/6070928338_aa10996f54_b.jpg",
+      title: "Computers For Remote Learning",
+    },
+    {
+      category: "Child",
+      date_end: 1629133200000,
+      id: 3,
+      pledged: 1500000,
+      profile_name: "Rina Rubina",
+      profile_photo:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Profile_Pic_Peepsys_Eye_2019.jpg/480px-Profile_Pic_Peepsys_Eye_2019.jpg",
+      target: 100000000,
+      thumbnail:
+        "https://www.luxurytravelmagazine.com/images/article/2014July/OLPC-2.jpg",
+      title: "One Laptop Per Child",
     },
   ],
 };
